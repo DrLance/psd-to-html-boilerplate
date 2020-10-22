a5be687dadaab00859d8e36e697f6a5ef90db434
Peter <megalance@gmail.com>
2020-10-22 14:10:50 +0400

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const {src, dest, series, watch} = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const gulpConnect = require('gulp-connect');
-const gulpLess = require('gulp-less');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
 const srcOutput = 'src';
 
 const out = 'public';
@@ -9,36 +12,34 @@ async function connect() {
   return gulpConnect.server({
     root: 'public',
     port: 1337,
-    livereload: true
+    livereload: true,
   });
 }
 
-
-async function less() {
-  return src('./src/less/**/*.less')
-    .pipe(gulpLess())
+async function sassCss() {
+  return src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass())
     .pipe(dest('./public/css/'))
     .pipe(gulpConnect.reload());
-};
-
+}
 
 async function html() {
-  return  src([srcOutput + '/*.html'])
+  return src([srcOutput + '/*.html'])
     .pipe(dest('./public/'))
     .pipe(gulpConnect.reload());
-};
+}
 
 async function js() {
-  return     src([srcOutput + '/js/*.js'])
+  return src([srcOutput + '/js/*.js'])
     .pipe(dest('./public/js/'))
     .pipe(gulpConnect.reload());
-};
-
+}
 
 async function watchFiles() {
-  watch([srcOutput + '/less/*.less', src + '/less/**/*.less'], less);
+  watch([srcOutput + '/scss/*.scss', src + '/scss/**/*.scss'], sassCss);
   watch([srcOutput + '/js/*.js', src + '/js/**/*.js'], js);
   return watch([srcOutput + '/*.html', srcOutput + '/html/**/*.html'], html);
-};
+}
 
-exports.default = series(connect, less, html, js, watchFiles);
+exports.default = series(connect, sassCss, html, js, watchFiles);
